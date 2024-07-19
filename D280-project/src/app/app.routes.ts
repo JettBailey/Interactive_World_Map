bc71b5ff0d7e3cfb9def0e1dec82a907daa7b9a3
Jett Bailey <jbai306@wgu.edu>
2024-07-19 15:29:48 -0700

    Step D Angular Routing

--- D280-project/src/app/app.routes.ts
+++ D280-project/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { WorldComponent } from './world/world.component';
 
+
 export const routes: Routes = [
-    {path: 'home', component: WorldComponent }
+    { path: '', redirectTo: '/world', pathMatch: 'full' },  // Redirect from root to /world
+    { path: 'world', component: WorldComponent }
 ];
